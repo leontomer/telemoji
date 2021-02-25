3f89d71c50187a22bb86a608e855cff0e73d1471
AlexKreinis <alexkre@wix.com>
2021-02-25 21:02:20 +0200

    done

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Toolbar>
           <Typography variant="h6">Telemoji </Typography>
           <Button color="inherit" href="/video-chat">
-            Demo1
+            Demo123
           </Button>
         </Toolbar>
       </AppBar>
