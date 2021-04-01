606fab7d828e4f8b8e56feae05e78ba1c6d5befa
Alex <alex.kreinis@walla.co.il>
2021-04-02 00:37:00 +0300

    done

--- client/src/Components/Navbar/AsyncSearch/AsyncSearch.tsx
+++ client/src/Components/Navbar/AsyncSearch/AsyncSearch.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import { fade, makeStyles } from "@material-ui/core/styles";
 
 interface FriendProps {
   name: string;
@@ -16,62 +15,6 @@ function sleep(delay = 0) {
 }
 
 export default function AsyncSearch() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      height: "6.4vh",
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-      display: "none",
-      [theme.breakpoints.up("sm")]: {
-        display: "block",
-      },
-    },
-    search: {
-      position: "relative",
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      "&:hover": {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(1),
-        width: "auto",
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100%",
-    },
-    inputRoot: {
-      color: "inherit",
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        width: "12ch",
-        "&:focus": {
-          width: "20ch",
-        },
-      },
-    },
-  }));
-
   const [open, setOpen] = React.useState(false);
   const [options, setOptions] = React.useState<FriendProps[]>([]);
   const loading = open && options.length === 0;
