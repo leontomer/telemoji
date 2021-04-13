37da5408868664f1883f7245db794217c7d4f850
AlexKreinis <alexkre@wix.com>
2021-04-13 14:24:41 +0300

    finished friend requeest

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -5,7 +5,7 @@ import addonReducer from "./addonReducer";
 import modelReducer from './modelReducer';
 import socketReducer from './socketReducer';
 import callReducer from './callReducer';
-import friendReducer from './friendReducer';
+// import friendReducer from './friendReducer';
 
 export default combineReducers({
   authReducer,
@@ -14,5 +14,5 @@ export default combineReducers({
   modelReducer,
   socketReducer,
   callReducer,
-  friendReducer
+  // friendReducer
 });
