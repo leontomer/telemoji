e57f698a267e4387316cb3116ab2bbb6bfb433dc
AlexKreinis <alexkre@wix.com>
2021-05-16 14:29:20 +0300

    added animations and socket call functions

--- client/src/Components/Dashboard/WelcomeBanner/WelcomeBanner.tsx
+++ client/src/Components/Dashboard/WelcomeBanner/WelcomeBanner.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./WelcomeBanner.scss";
 import welcomeBannerSvg from "../../../svgs/welcomeBannerSvg.svg";
+import sunSvg from '../../../svgs/sun.svg';
+import nightSvg from '../../../svgs/night.svg';
+import morningSvg from '../../../svgs/morning.svg';
 import { useSelector } from "react-redux";
 import { useSpring, animated } from 'react-spring'
 
@@ -10,6 +13,7 @@ const WelcomeBanner = () => {
   var today = new Date()
   var curHr = today.getHours()
   const [welcomeMessage, setWelcomeMessage] = useState('');
+  const [welcomeImage, setWelcomeImage] = useState('');
   const styles = useSpring({
     loop: true,
     from: { rotateZ: 0 },
@@ -20,34 +24,36 @@ const WelcomeBanner = () => {
   useEffect(() => {
     if (curHr < 12) {
       setWelcomeMessage('Good morning')
+      setWelcomeImage(morningSvg)
     } else if (curHr < 18) {
       setWelcomeMessage('Good afternoon')
+      setWelcomeImage(sunSvg)
     } else {
       setWelcomeMessage('Good evening')
+      setWelcomeImage(nightSvg)
     }
   }, [])
 
 
   return (
-    <div className="WelcomeBannerContainer">
-      <div style={{ color: "white", fontSize: 30 }}>
-        {firstName && (
-          <h1>
-            {welcomeMessage}, {firstName.charAt(0).toUpperCase() + firstName.slice(1)}
-          </h1>
-        )}
+    <div className="WelcomeBannerContainer" >
+      <div style={{ color: "white", display: 'flex', alignItems: 'center' }}>
         <div>
+          {firstName && (
+            <h1>
+              {welcomeMessage}, {firstName.charAt(0).toUpperCase() + firstName.slice(1)}
+            </h1>
+          )}
           {welcomeMessage && <p
-            style={{
-              color: "white",
-              fontSize: 18,
-              marginLeft: 120,
-              marginTop: 10
-            }}
+            className="subTitle"
           >
             Have a chat with your friends!
         </p>}
         </div>
+        <div style={{ marginLeft: 50 }}>
+          <img src={welcomeImage} height="100px" />
+        </div>
+
       </div>
 
       <animated.div style={{
