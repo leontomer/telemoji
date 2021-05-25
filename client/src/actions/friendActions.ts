55ca2b31ebed61f971047aa99f2d6ee2aab2d091
tomdamri1 <tomda@wix.com>
2021-05-25 18:10:47 +0300

    [telemoji] | added - go back to my profile in friendlist

--- client/src/actions/friendActions.ts
+++ client/src/actions/friendActions.ts
@@ -1,134 +1,122 @@
 import axios from "axios";
 import {
-    GET_FRIEND_LIST,
-    SET_FRIEND_REQUESTS,
-    SET_FRIEND_IN_FOCUS,
-    SET_SELECTED_CALL_HISTORY_STATS,
-    CLEAR_SELECTED_CALL_HISTORY_STATS
+  GET_FRIEND_LIST,
+  SET_FRIEND_REQUESTS,
+  SET_FRIEND_IN_FOCUS,
+  SET_SELECTED_CALL_HISTORY_STATS,
+  CLEAR_SELECTED_CALL_HISTORY_STATS,
 } from "./types";
 import { FriendProps } from "../reducers/authReducer";
 
 const baseRoute = "/api/friends/";
 
-
 export const sendFriendRequest = () => async (_, getState) => {
-    const friendReuqestID = getState().friendReducer.friendInFocus._id;
-    try {
-        await axios.post(`${baseRoute}addfriend`, {
-            friendReuqestID,
-        });
-    } catch (error) {
-        console.error(error);
-    }
+  const friendReuqestID = getState().friendReducer.friendInFocus._id;
+  try {
+    await axios.post(`${baseRoute}addfriend`, {
+      friendReuqestID,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const removeFriend = ({
-    userFriendId,
-}: {
-    userFriendId: string;
-}) => async (dispatch) => {
+export const removeFriend =
+  ({ userFriendId }: { userFriendId: string }) =>
+  async (dispatch) => {
     try {
-        await axios.post(`${baseRoute}removeFriend`, {
-            userFriendId,
-        });
-        dispatch(getFriendList())
-
+      await axios.post(`${baseRoute}removeFriend`, {
+        userFriendId,
+      });
+      dispatch(getFriendList());
     } catch (err) {
-        console.log(err);
+      console.log(err);
     }
-};
+  };
 
 export const updatePendingFriendRequests = () => async (dispatch, getState) => {
-    try {
-        const email = getState().authReducer.user.email
-        const res = await axios.get(`${baseRoute}pendingFriendRequests`, {
-            params: { email },
-        });
-        const friendRequests = res.data.user.friendRequests;
-        dispatch({ type: SET_FRIEND_REQUESTS, payload: friendRequests });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const email = getState().authReducer.user.email;
+    const res = await axios.get(`${baseRoute}pendingFriendRequests`, {
+      params: { email },
+    });
+    const friendRequests = res.data.user.friendRequests;
+    dispatch({ type: SET_FRIEND_REQUESTS, payload: friendRequests });
+  } catch (err) {
+    console.log(err);
+  }
 };
-export const approvePendingFriendRequest = ({
-    userFriendEmail,
-}: {
-    userFriendEmail: string;
-}) => async (dispatch) => {
+export const approvePendingFriendRequest =
+  ({ userFriendEmail }: { userFriendEmail: string }) =>
+  async (dispatch) => {
     await axios.post(`${baseRoute}approveFriend`, {
-        userFriendEmail,
+      userFriendEmail,
     });
-    dispatch(updatePendingFriendRequests())
-    dispatch(getFriendList())
-};
+    dispatch(updatePendingFriendRequests());
+    dispatch(getFriendList());
+  };
 
-export const rejectPendingFriendRequest = ({
-    userFriendEmail,
-}: {
-    userFriendEmail: string;
-}) => async (dispatch) => {
+export const rejectPendingFriendRequest =
+  ({ userFriendEmail }: { userFriendEmail: string }) =>
+  async (dispatch) => {
     await axios.post(`${baseRoute}rejectFriend`, {
-        userFriendEmail,
+      userFriendEmail,
     });
-    dispatch(updatePendingFriendRequests())
-};
-
+    dispatch(updatePendingFriendRequests());
+  };
 
 export const getFriendList = () => async (dispatch) => {
-    try {
-        const res = await axios.get(`${baseRoute}friendList`);
-        const { data } = res;
-        dispatch({ type: GET_FRIEND_LIST, payload: data.friendList });
-    } catch (e) {
-        console.warn(e);
-    }
+  try {
+    const res = await axios.get(`${baseRoute}friendList`);
+    const { data } = res;
+    dispatch({ type: GET_FRIEND_LIST, payload: data.friendList });
+  } catch (e) {
+    console.warn(e);
+  }
 };
 
-export const setFriendInFocus = (friend: FriendProps) => (dispatch) => {
-    dispatch({
-        type: SET_FRIEND_IN_FOCUS,
-        payload: friend,
-    });
+export const setFriendInFocus = (friend: FriendProps | null) => (dispatch) => {
+  dispatch({
+    type: SET_FRIEND_IN_FOCUS,
+    payload: friend,
+  });
 };
 
 export const friendListListener = () => async (dispatch, getState) => {
-    const socket = getState().socketReducer.socket;
+  const socket = getState().socketReducer.socket;
 
-    socket.on("friendListUpdate", () => {
-        dispatch(getFriendList());
-    });
+  socket.on("friendListUpdate", () => {
+    dispatch(getFriendList());
+  });
 };
 
-export const pendingFriendRequestsListener = () => async (dispatch, getState) => {
+export const pendingFriendRequestsListener =
+  () => async (dispatch, getState) => {
     const socket = getState().socketReducer.socket;
 
     socket.on("friendRequestListUpdate", () => {
-        dispatch(updatePendingFriendRequests());
+      dispatch(updatePendingFriendRequests());
     });
-}
-
-
+  };
 
 export const getCallStats = (callId: string) => async (dispatch) => {
-    try {
-        console.log('entered!');
-        const res = await axios.get(`${baseRoute}call-stats/${callId}`);
-        console.log('resdata is', res.data);
-        if (res.data) {
-            dispatch({
-                type: SET_SELECTED_CALL_HISTORY_STATS,
-                payload: res.data
-            })
-        }
-
-    } catch (error) {
-        console.error(error)
+  try {
+    console.log("entered!");
+    const res = await axios.get(`${baseRoute}call-stats/${callId}`);
+    console.log("resdata is", res.data);
+    if (res.data) {
+      dispatch({
+        type: SET_SELECTED_CALL_HISTORY_STATS,
+        payload: res.data,
+      });
     }
-}
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const clearSelectedCallStats = () => (dispatch) => {
-    dispatch({
-        type: CLEAR_SELECTED_CALL_HISTORY_STATS
-    })
-}
-
+  dispatch({
+    type: CLEAR_SELECTED_CALL_HISTORY_STATS,
+  });
+};
