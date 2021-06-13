2dfc5fd441d2edd3774924659800afd91d99539e
leontomer <41841316+leontomer@users.noreply.github.com>
2021-06-13 20:34:46 +0300

    make it better

--- client/src/Components/LoginPage/GoogleLogin/GoogleLogin.tsx
+++ client/src/Components/LoginPage/GoogleLogin/GoogleLogin.tsx
@@ -11,47 +11,47 @@ import { snackbarType } from "../../../Common/dataTypes";
 import lan from "../../../Languages/Languages.json";
 
 function GoogleLoginHooks({ goToDashboard }) {
-  // @ts-ignore
-  const globalLanguage = useSelector((state) => state.LanguageReducer.language);
-  const [language, setLocalLanguage] = React.useState(globalLanguage);
-  useEffect(() => {
-    setLocalLanguage(globalLanguage);
-  }, [globalLanguage]);
-  const { startLoading, finishLoading } = useLoader();
-  const isAuthenticated = useSelector(
-    // @ts-ignore
-    (state) => state.authReducer.isAuthenticated
-  );
-  useLayoutEffect(() => {
-    if (isAuthenticated) {
-      finishLoading();
-      goToDashboard();
-    }
-  }, [isAuthenticated]);
+  // // @ts-ignore
+  // const globalLanguage = useSelector((state) => state.LanguageReducer.language);
+  // const [language, setLocalLanguage] = React.useState(globalLanguage);
+  // useEffect(() => {
+  //   setLocalLanguage(globalLanguage);
+  // }, [globalLanguage]);
+  // const { startLoading, finishLoading } = useLoader();
+  // const isAuthenticated = useSelector(
+  //   // @ts-ignore
+  //   (state) => state.authReducer.isAuthenticated
+  // );
+  // useLayoutEffect(() => {
+  //   if (isAuthenticated) {
+  //     finishLoading();
+  //     goToDashboard();
+  //   }
+  // }, [isAuthenticated]);
 
-  const dispatch = useDispatch();
-  const onSuccess = (res) => {
-    try {
-      dispatch(loginWithGoogle(res.tokenId));
-      refreshTokenSetup(res);
-      finishLoading();
-    } catch (error) {
-      finishLoading();
-      dispatch(setMessage(error.msg, snackbarType.error));
-    }
-  };
-  const onFailure = (res) => {
-    dispatch(setMessage(res, snackbarType.error));
-    finishLoading();
-  };
+  // const dispatch = useDispatch();
+  // const onSuccess = (res) => {
+  //   try {
+  //     dispatch(loginWithGoogle(res.tokenId));
+  //     refreshTokenSetup(res);
+  //     finishLoading();
+  //   } catch (error) {
+  //     finishLoading();
+  //     dispatch(setMessage(error.msg, snackbarType.error));
+  //   }
+  // };
+  // const onFailure = (res) => {
+  //   dispatch(setMessage(res, snackbarType.error));
+  //   finishLoading();
+  // };
 
-  const { signIn } = useGoogleLogin({
-    onSuccess,
-    onFailure,
-    clientId,
-    isSignedIn: false,
-    accessType: "offline",
-  });
+  // const { signIn } = useGoogleLogin({
+  //   onSuccess,
+  //   onFailure,
+  //   clientId,
+  //   isSignedIn: false,
+  //   accessType: "offline",
+  // });
 
   return (
     <div>hi</div>
